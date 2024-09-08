d6b2b474daeb83f36f62bb1e3d5eeb3e62cc4844
Pallavi1502 <137083391+Pallavi1502@users.noreply.github.com>
2024-09-08 11:16:33 +0530

    bug fixs

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+
+require('dotenv').config();
 const express = require("express");
 const app = express();
 
@@ -5,17 +7,19 @@ const userRoutes = require("./routes/user");
 const profileRoutes = require("./routes/profile");
 const paymentRoutes = require("./routes/payments");
 const courseRoutes = require("./routes/course");
+const contactUsRoute = require("./routes/Contact");
+
 
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const {cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
+// dotenv.config();
 
 
-dotenv.config();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 //database connect
 database.connect();
@@ -43,6 +47,7 @@ app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/payment", paymentRoutes);
+app.use("/api/v1/reach", contactUsRoute);
 
 
 
@@ -55,7 +60,7 @@ app.get("/", (req, res) => {
 	});
 });
 
-app.listen(PORT, () => {
-	console.log(`App is running at ${PORT}`)
+app.listen(port, () => {
+	console.log(`App is running at ${port}`)
 })
 
