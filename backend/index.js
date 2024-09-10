9100449b19eff849cb2291b0e680870cf676a2c4
Pallavi1502 <137083391+Pallavi1502@users.noreply.github.com>
2024-09-10 15:20:13 +0530

    added setup for deploy on render

--- backend/index.js
+++ backend/index.js
@@ -57,9 +57,9 @@ app.use("/api/v1/contact", contactUsRoute);
 
 const __dirname1= path.resolve()
 if(process.env.NODE_ENV === 'production'){
-	app.use(express.static(path.join(__dirname1, '../build')))
+	app.use(express.static(path.join(__dirname1, '/frontend/build')))
 	app.get('*', (req,res) =>{
-		res.sendFile(path.resolve(__dirname1, "../build", "index.html"))
+		res.sendFile(path.resolve(__dirname1,"frontend", "build", "index.html"))
 	})
 
 } else{
