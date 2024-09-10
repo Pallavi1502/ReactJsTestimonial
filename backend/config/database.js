5d004c05a9f34b4eb94c08713946fd771218e2ef
Pallavi1502 <137083391+Pallavi1502@users.noreply.github.com>
2024-09-10 15:28:08 +0530

    added setup for deploy on render

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,6 @@
 const mongoose= require("mongoose");
 require("dotenv").config({path: 'backend/.env'});
 
-console.log("printing mongo url", process.env.MONGODB_URL)
-console.log("printing mongo url", process.env.CLOUD_NAME)
 
 
 exports.connect = async () => {
